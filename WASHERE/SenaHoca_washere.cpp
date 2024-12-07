b8526e4ebef969428993831d93b59a6d0716d340
Berre Sena KIRAÇ <berresenakirac71@gmail.com>
2024-12-07 23:20:39 +0300

    Create SenaHoca_washere.cpp

---
+++ WASHERE/SenaHoca_washere.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+
+int main() {
+    std::cout << "Sena Hoca Was Here" << std::endl;
+    std::cout << "Arkadaşlar eklemek istediğiniz mesaj vs. buraya yazın ben okuyacağım" << std::endl;
+    return 0; 
+}
